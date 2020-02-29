1e165520af3528639a125ecae7c453f7717d0d1b
jiangdonghan <2641635339@qq.com>
2020-02-29 18:43:53 +1000

    add handleInputChange

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,9 +1,39 @@
-import React, {Component} from "react";
-
+import React, {Component, Fragment} from "react";
+// react 16 provides Fragment to replace outside div
 class TodoList extends Component {
+  constructor(props){
+    //super调用父类的构造函数
+    super(props);
+    this.state = {
+      inputValue: '',
+      list: []
+    }
+    this.handleInputChange = this.handleInputChange.bind(this)
+  }
+  handleInputChange (e) {
+    console.log(e.target.value)
+    this.setState({
+      inputValue: e.target.value
+    })
+  }
   render() {
     return (
-      <div>todolist</div>
+      <Fragment>
+        <div>
+        <input 
+          value={this.state.inputValue}
+          onChange={this.handleInputChange}
+        /><button onClick={this.handleButtonClick}>submit</button>
+        </div>
+        <ul>
+          <li>
+            learn React
+          </li>
+          <li>
+            learn English
+          </li>
+        </ul>
+      </Fragment>
     )
   }
 }
