9ed4fee4a395e028d0008c3cc459267c8f0d143e
jiangdonghan <2641635339@qq.com>
2020-02-29 21:53:00 +1000

    finish add todo list function

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,4 +1,5 @@
 import React, {Component, Fragment} from "react";
+import './style.css';
 // react 16 provides Fragment to replace outside div
 class TodoList extends Component {
   constructor(props){
@@ -6,32 +7,46 @@ class TodoList extends Component {
     super(props);
     this.state = {
       inputValue: '',
-      list: []
+      list: ['dd','ss']
     }
     this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleButtonClick = this.handleButtonClick.bind(this)
   }
   handleInputChange (e) {
-    console.log(e.target.value)
     this.setState({
       inputValue: e.target.value
+    })
+  }
+  handleButtonClick () {
+    this.setState({
+      list:[...this.state.list, this.state.inputValue],
+      inputValue: ''
+    })
+  }
+  handleItemDelete (index) {
+    //immutable
+    //states are not allowed to be changed
+    const list = [...this.state.list]
+    list.splice(index,1)
+    this.setState({
+      list: list  //or just list
     })
   }
   render() {
     return (
       <Fragment>
+        {//fragment is also a comoponet
+        }
         <div>
-        <input 
+        <input className = "input"
           value={this.state.inputValue}
           onChange={this.handleInputChange}
         /><button onClick={this.handleButtonClick}>submit</button>
         </div>
         <ul>
-          <li>
-            learn React
-          </li>
-          <li>
-            learn English
-          </li>
+          {this.state.list.map((item,index)=>{
+            return <li key={index} onClick={this.handleItemDelete.bind(this, index)}>{item}</li>
+          })}
         </ul>
       </Fragment>
     )
