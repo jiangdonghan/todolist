a679a57f09c357a9b48fd1223e7677a3d5ca391e
jiangdonghan <2641635339@qq.com>
2020-04-17 00:34:58 +0800

    learn reducer and action

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,7 +2,20 @@ const defaultState = {
   inputValue: 'ddd',
   list: ['1','2']
 }
+//reducer可以接受state， 但是绝不能修改state
 export default (state = defaultState, action) => {
   //state 整个store仓库里存储的数据
+  console.log(state,action)
+  if (action.type === 'change_input_value'){
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue = action.value; 
+    return newState
+  }
+  if (action.type === 'add_todo_item'){
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.list.push(newState.inputValue); 
+    newState.inputValue = ''
+    return newState
+  }
   return state;
 }
